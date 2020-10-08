0cafecc06c259d9a296acaae9485957e3bcaad75
romigos <bolist@mail.ru>
2020-10-08 07:49:53 +0300

    create new

--- app/controllers/fruits_controller.rb
+++ app/controllers/fruits_controller.rb
@@ -1,5 +1,30 @@
+# frozen_string_literal: true
+
 class FruitsController < ApplicationController
+  def index
+    @fruits = Fruit.all
+  end
+
   def new
     @fruit = Fruit.new
+  end
+
+  def show
+    @fruit = Fruit.find(params[:id])
+  end
+
+  def create
+    @fruit = Fruit.new(fruit_params)
+    if @fruit.save
+      redirect_to @fruit
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def fruit_params
+    params.require(:fruit).permit(:name, :color, :quantity)
   end
 end
